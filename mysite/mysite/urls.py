ac273301527fc2893729fc1976bac5b163928588
xochiltramirez <xochilt.ramirez@gmail.com>
2016-11-14 07:45:58 -0800

    primer commit

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,26 @@
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+
+from django.conf.urls import include, url
+from django.contrib import admin
+from polls import views
+#from polls.views import registrationView
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^test/$', views.test, name='test'), 
+    url(r'^accounts/login/$',  'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',  {'next_page': '/logout/'}),
+    url(r'^accounts/', include('registration.backends.hmac.urls')),
+    url(r'^logout/', views.logoutPage, name='logout'),
+]
